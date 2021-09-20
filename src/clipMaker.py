66f0cf1852addc42c614ee9b2f87658de930005f
simonMerci <nomismercier@gmail.com>
2021-09-20 09:42:45 -0400

    refactored clipMaker, now no longer using abs paths

--- src/clipMaker.py
+++ src/clipMaker.py
@@ -1,8 +1,15 @@
 import os
-
+from pathlib import Path
 #DEBUT CLASSE
 class ClipMaker:
-    
-    def __init__(self, framerate, number):
-        os.system("C:/Users/mison/PycharmProjects/Robots/venv/Lib/site-packages/imageio_ffmpeg/binaries/ffmpeg-win32-v4.1.exe -r 5 -framerate "+ framerate +" -s 1920x1080 -i C:/Users/mison/PycharmProjects/Robots/Reddit/Video/VideoScreenshots/comment"+number+".png -i C:/Users/mison/PycharmProjects/Robots/Reddit/Video/VideoTTS/comment"+number+".mp3 -pix_fmt yuv420p -vcodec libx264 -acodec copy -y C:/Users/mison/PycharmProjects/Robots/Reddit/Video/VideoClips/clip"+number+".mp4")
+    def __init__(self):
+        self.root_path = Path(__file__).resolve().parent.parent
+        self.ffmpeg_path = f"{self.root_path}/venv/site-packages/imageio_ffmpeg/binaries/ffmpeg-win32-v4.1.exe"
+        self.assets_path = f"{self.root_path}/assets"
+    def __call__(self, framerate, number):
+        os.system(f"{self.ffmpeg_path} -r 5 -framerate {framerate} -s 1920x1080 -i {self.assets_path}/pictures/comment{number}.png -i {self.assets_path}/audio/tts{number}.mp3 -pix_fmt yuv420p -vcodec libx264 -acodec copy -y {self.assets_path}/clips/clip{number}.mp4")
+
+
+
+
 #FIN CLASSE
